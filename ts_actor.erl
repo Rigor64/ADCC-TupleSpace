38934fedbd57b76a9877aa61d1f9731ba3ec7199
Rigor64 <matteoleopizzi99@gmail.com>
2024-12-06 20:52:08 +0100

    Add tmpFunc, inFunc, rdFunc Update server receive

--- ts_actor.erl
+++ ts_actor.erl
@@ -38,10 +38,29 @@ server(WL, TS, WaitQueue) ->
 			server(WL, TS, NWQ);
 		
 		% Handle ETS non-destructive read
-		{rd, Pid, Pattern} -> ok, server(WL, TS, WaitQueue);
+		{rd, Pid, Pattern} -> % Check if is in the white list 
+			Present = inWhiteList(WL, Pid),
+			case Present of 
+				% If autorized try to read and wait otherwise
+				true ->
+					NWQ = tmpFunc(TS, {rd, Pid, Pattern}, WaitQueue);
+				_ -> 
+					NWQ = WaitQueue
+			end, 
+			server(WL, TS, NWQ);
 		
 		% Handle ETS write, WaitQueue removal
-		{out, Pid, Tuple} -> ok, server(WL, TS, WaitQueue);
+		{out, Pid, Tuple} -> 
+			Present = inWhiteList(WL, Pid),
+			case Present of 
+				% If autorized try to read and wait otherwise
+				true ->
+					ets:insert(TS, Tuple),
+					NWQ = consumeWQ(TS, WaitQueue);
+				_ -> 
+					NWQ = WaitQueue
+			end, 
+			server(WL, TS, NWQ);
 
 		% Handle add node
 		{add_node, Pid, Node} -> addNode(WL, Node), Pid!{ok, "Il nodo ~p è stato aggiunto", Node}, server(WL, TS, WaitQueue);
@@ -93,7 +112,7 @@ inWhiteList(WL, Node) ->
 .
 
 
-tmpFunc(TS, {in, Pid, Pattern}, WaitQueue) -> 
+inFunc(TS, {in, Pid, Pattern}, WaitQueue) -> 
 	% Control  on Pattern Matching
 	Res = ets:lookup(TS, Pattern),
 	case Res of
@@ -106,5 +125,51 @@ tmpFunc(TS, {in, Pid, Pattern}, WaitQueue) ->
 			ets:delete(TS, H),
 			NWQ = WaitQueue
 	end,
+	NWQ
+.
+
+rdFunc(TS, {rd, Pid, Pattern}, WaitQueue) -> 
+	% Control  on Pattern Matching
+	Res = ets:lookup(TS, Pattern),
+	case Res of
+		% If not in the tuple space add to waitqueue
+		[] ->
+			NWQ = WaitQueue ++ {rd, Pid, Pattern};
+		% Else return the element 
+		[H | _T] ->
+			Pid!{ok, H},
+			NWQ = WaitQueue
+	end,
+	NWQ
+.
+
+tmpFunc(TS, {Type, Pid, Pattern}, WaitQueue) -> 
+	% Control  on Pattern Matching
+	Res = ets:lookup(TS, Pattern),
+	case Res of
+		% If not in the tuple space add to waitqueue
+		[] ->
+			NWQ = WaitQueue ++ {Type, Pid, Pattern};
+		% Else return the element 
+		[H | _T] ->
+			Pid!{ok, H},
+			case Type of
+				in -> ets:delete(TS, H);
+				_ -> ok
+			end,
+			NWQ = WaitQueue
+	end,
+	NWQ
+.
+
+consumeWQ(TS, WQ) ->
+	NWQ = lists:foldr(
+		fun({Type, Pid, Pattern}, Acc) -> 
+			NewAcc = tmpFunc(TS, {Type, Pid, Pattern}, Acc),
+			NewAcc
+		end,
+		[],
+		WQ	
+	),
 	NWQ
 .
