2c869460abb93ad6544988f3e66e2ce4900a4fe6
Nicholas Kania <nicholas.nick.kania.development@gmail.com>
2024-12-12 13:22:37 +0100

    Software Update
    > Added full implementation of the gen_server version of Tuple Space

--- gs/ts.erl
+++ gs/ts.erl
@@ -3,40 +3,44 @@
 
 % Export all invokable functions
 -export([
-	% Interfaces 1/3
-	new/1,
-	in/2,
-	rd/2,
-	out/2,
+    % Interfaces 1/3
+    new/1,
+    in/2,
+    rd/2,
+    out/2,
 
-	% Interfaces 2/3
-	in/3,
-	rd/3,
+    % Interfaces 2/3
+    in/3,
+    rd/3,
 
-	% Interfaces 3/3
-	addNode/2,
-	removeNode/2,
-	nodes/1
+    % Interfaces 3/3
+    addNode/2,
+    removeNode/2,
+    nodes/1,
+
+    % Testing
+    list/1,
+    wq/1
 ]).
 
 
-init(Name) ->
-	% Enable trap_exit management
-	erlang:process_flag(trap_exit, true),
-
-	% Start server
-	gen_server:start_link({global, Name}, tsb, [], []),
-
-	supervise(Name)
-.
-
-supervise(Name) ->
-	receive
-		{'EXIT', _Pid, _Reason} ->
-			gen_server:start_link({global, Name}, tsb, [], []),
-			supervise(Name)
-	end
-.
+%init(Name) ->
+%	% Enable trap_exit management
+%	erlang:process_flag(trap_exit, true),
+%
+%	% Start server
+%	gen_server:start_link({global, Name}, tsb, [], []),
+%
+%	supervise(Name)
+%.
+%
+%supervise(Name) ->
+%	receive
+%		{'EXIT', _Pid, _Reason} ->
+%			gen_server:start_link({global, Name}, tsb, [], []),
+%			supervise(Name)
+%	end
+%.
 
 
 %%%
@@ -45,150 +49,110 @@ supervise(Name) ->
 
 % Creates a new tuple space with Name
 new(Name) ->
-	spawn(
-		node(),
-		% spawn/2 to not export the init function of the supervisor
-		fun() ->
-			init(Name)
-		end
-	),
-	io:format("New tuple space created: ~p\n", [Name])
+    gen_server:start({global, Name}, tsb, [atom_to_list(Name)], [{hibernate_after, 10000}]),
+    io:format("New tuple space created: ~p\n", [Name]),
+    addNode(Name, self())
 .
 
 
 
 % Read Pattern from the tuple space TS (desctructive)
 in(TS, Pattern) -> % Use matching specification
-	gen_server:call({global, TS}, {in, Pattern}, infinity)
+    in(TS, Pattern, infinity)
 .
 
 % Read Pattern from the tuple space TS (desctructive)
 in(TS, Pattern, Timeout) ->
-	gen_server:call({global, TS}, {in, Pattern}, Timeout)
+    try gen_server:call({global, TS}, {in, Pattern}, Timeout) of
+        Response -> Response
+    catch
+        throw:timeout ->
+            %% Handle timeout error specifically
+            gen_server:cast({global, TS}, {abort, {in, self(), Pattern}}),
+            
+            {err, timeout};
+        error: Reason ->
+            %% Handle other errors
+            gen_server:cast({global, TS}, {abort, {in, self(), Pattern}}),
+            
+            {err, Reason};
+        exit: Reason ->
+            %% Handle exits
+            gen_server:cast({global, TS}, {abort, {in, self(), Pattern}}),
+            
+            {Type, _} = Reason,
+            case Type of
+                timeout -> {err, timeout};
+                _ -> {err, Reason}
+            end
+    end
 .
 
 
 
 % Read Pattern from the tuple space TS (non-desctructive)
 rd(TS, Pattern) -> % Use matching specification
-	gen_server:call({global, TS}, {rd, Pattern}, infinity)
+    rd(TS, Pattern, infinity)
 .
 
 % Read Pattern from the tuple space TS (non-desctructive)
 rd(TS, Pattern, Timeout) ->
-	gen_server:call({global, TS}, {rd, Pattern}, Timeout)
+    try gen_server:call({global, TS}, {rd, Pattern}, Timeout) of
+        Response -> Response
+    catch
+        throw:timeout ->
+            %% Handle timeout error specifically
+            gen_server:cast({global, TS}, {abort, {rd, self(), Pattern}}),
+            {err, timeout};
+        error: Reason ->
+            %% Handle other errors
+            gen_server:cast({global, TS}, {abort, {rd, self(), Pattern}}),
+            {err, Reason};
+        exit: Reason ->
+            %% Handle exits
+            gen_server:cast({global, TS}, {abort, {rd, self(), Pattern}}),
+            
+            {Type, _} = Reason,
+            case Type of
+                timeout -> {err, timeout};
+                _ -> {err, Reason}
+            end
+    end
 .
+
+
+
+
+
+
 
 
 
 % Write Tuple in the tuple space TS
 out(TS, Tuple) ->
-	gen_server:cast({global, TS}, {out, Tuple})
+    gen_server:cast({global, TS}, {out, self(), Tuple})
 .
 
 
 
 % Add Node to the TS, so Node can access to all tuples of TS
 addNode(TS, Node) ->
-	gen_server:cast({global, TS}, {add_node, Node})
+    gen_server:cast({global, TS}, {add_node, Node})
 .
 
 % Remove Node from the TS
 removeNode(TS, Node) ->
-	gen_server:cast({global, TS}, {rm_node, Node})
+    gen_server:cast({global, TS}, {rm_node, Node})
 .
 
 % Get list of nodes who can access to the tuple space
 nodes(TS) ->
-	gen_server:call({global, TS}, {nodes})
+    gen_server:call({global, TS}, {nodes})
 .
 
+%%%
+%%%
+%%%
 
-
-
-
-
-
-
-
-
-
-
-%new(Name) ->
-%	global:register_name(Name, spawn(node(), tsm, init, [])),
-%	io:format("New tuple space created: ~p\n", [Name]),
-%	addNode(Name, self()),
-%	ok
-%.
-
-% Read Pattern from the tuple space TS (desctructive)
-%in(TS, Pattern) -> % Use matching specification
-%	Ret = in(TS, Pattern, infinity),
-%	Ret
-%.
-
-% Read Pattern from the tuple space TS (non-desctructive)
-%rd(TS, Pattern) -> % Use matching specification
-%	Ret = rd(TS, Pattern, infinity),
-%	Ret
-%.
-%
-%% Write Tuple in the tuple space TS
-%out(TS, Tuple) ->
-%	global:whereis_name(TS)!{out, self(), Tuple},
-%	ok
-%.
-%
-%% Read Pattern from the tuple space TS (desctructive)
-%in(TS, Pattern, Timeout) ->
-%	% Send in request
-%	global:whereis_name(TS)!{in, self(), Pattern},
-%
-%	% Wait for message
-%	receive
-%		{ok, Tuple} -> {ok, Tuple}
-%	after
-%		Timeout -> {err, timeout}
-%	end
-%.
-%
-%% Read Pattern from the tuple space TS (non-desctructive)
-%rd(TS, Pattern, Timeout) ->
-%	% Send in request
-%	global:whereis_name(TS)!{rd, self(), Pattern},
-%
-%	% Wait for message
-%	receive
-%		{ok, Tuple} -> {ok, Tuple}
-%	after
-%		Timeout -> {err, timeout}
-%	end
-%.
-%
-%
-%
-%
-%% Add Node to the TS, so Node can access to all tuples of TS
-%addNode(TS, Node) ->
-%	% Send in request
-%	global:whereis_name(TS)!{add_node, self(), Node}
-%.
-%
-%% Remove Node from the TS
-%removeNode(TS, Node) ->
-%	% Send in request
-%	global:whereis_name(TS)!{remove_node, self(), Node}
-%.
-%
-%% Get list of nodes who can access to the tuple space
-%%nodes(TS) ->
-%%	% Send nodes request
-%%	global:whereis_name(TS)!{nodes, self()},
-%%	receive
-%%		{ok, List} -> List
-%%	after
-%%		5000 -> {err, timeout}
-%%	end
-%%.
-%
-%nodes(TS) -> gen_server:call(TS, {}, 5000).
+list(TS) -> gen_server:call({global, TS}, {list}).
+wq(TS) -> gen_server:call({global, TS}, {wq}).
--- gs/tsb.erl
+++ gs/tsb.erl
@@ -1,61 +1,218 @@
 % Tuple Space Behaviors
 -module(tsb).
 -behaviour(gen_server).
--export([init/1, handle_call/3, handle_cast/2]).
+-export([
+    init/1,
+    handle_info/2,
+    handle_call/3,
+    handle_cast/2
+]).
 
-% Init server state
-init({Name}) ->
-	% Enable trap_exit management
-	erlang:process_flag(trap_exit, true),
-	
-	%io:format("Debug print - INIT PID (~p)\n", [self()]),
 
-	% Obtain reference for tables
-	% Create DETS for filesystem sync
-	SyncFileRef = dets:open_file(Name),
 
-	% Create ETS whitelist
-	WhiteListRef = ets:new(whitelist, [set, private]),
-	% Create ETS space
-	TupleSpaceRef = ets:new(space, [set, private]),
+%%% Init server state
+init([Name]) ->
+    % Enable trap_exit management
+    erlang:process_flag(trap_exit, true),
+    
+    % Obtain reference for tables
+    % Create DETS for filesystem sync
+    {ok, SyncFileRef} = dets:open_file(Name, []),
 
-	% Create WaitQueue for blocking reads
-	WaitQueue = [],
+    % Create ETS whitelist
+    WhiteListRef = ets:new(whitelist, [set, private]),
+    % Create ETS space
+    TupleSpaceRef = ets:new(space, [set, private]),
 
-	{ok, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}}
+    % Load tuple space
+    dets:to_ets(SyncFileRef, TupleSpaceRef),
+
+    % Create WaitQueue for blocking reads
+    WaitQueue = [],
+
+    {ok, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}}
 .
 
 
 
-% Define sync endpoints behaviors
+%%% Handle infos
+handle_info(timeout, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}) ->
+    % Handle dets sync
+    ets:to_dets(TupleSpaceRef, SyncFileRef),
+
+    {noreply, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}, hibernate};
+
+handle_info({'EXIT', Pid, _Reason}, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}) ->
+    % Handle dets sync
+    removeFromWhiteList(WhiteListRef, Pid),
+    ClearedQueue = removePendingRequests(WaitQueue, Pid),
+
+    {noreply, {SyncFileRef, WhiteListRef, TupleSpaceRef, ClearedQueue}}
+.
+
+%%% Define sync endpoints behaviors
+%%% TEST
+handle_call({list}, From, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}) ->
+    {reply, {ets:tab2list(TupleSpaceRef)}, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}};
+handle_call({wq}, From, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}) ->
+    {reply, {WaitQueue}, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}};
+%%%
 handle_call({in, Pattern}, From, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}) ->
-	{reply, {ok}, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}};
+    {Pid, _} = From,
+    Present = inWhiteList(WhiteListRef, Pid),
+    io:format("DEBUG PRINT - inWhiteList (in) (~p)\n", [Present]),
+    case Present of
+        % If autorized try to read and wait otherwise
+        true ->
+            NewWaitQueue = tryProcessRequest({in, From, Pattern}, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue});
+        false ->
+            NewWaitQueue = WaitQueue
+    end,
+    {noreply, {SyncFileRef, WhiteListRef, TupleSpaceRef, NewWaitQueue}};
 
 handle_call({rd, Pattern}, From, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}) ->
-	{reply, {ok}, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}};
+    {Pid, _} = From,
+    Present = inWhiteList(WhiteListRef, Pid),
+    %io:format("DEBUG PRINT - inWhiteList (rd) (~p)\n", [Present]),
+    case Present of
+        % If autorized try to read and wait otherwise
+        true ->
+            NewWaitQueue = tryProcessRequest({rd, From, Pattern}, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue});
+        false ->
+            NewWaitQueue = WaitQueue
+    end,
+    {noreply, {SyncFileRef, WhiteListRef, TupleSpaceRef, NewWaitQueue}};
 
+% List of connected nodes
 handle_call({nodes}, _From, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}) ->
-	Acc = ets:foldr(
-		fun({Elem}, Acc) ->
-			Acc ++ [Elem]	
-		end,
-		[],
-		WhiteListRef
-	),
-	
-	{reply, {ok, Acc}, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}}
+    Acc = ets:foldr(
+        fun({Elem}, Acc) ->
+            Acc ++ [Elem]	
+        end,
+        [],
+        WhiteListRef
+    ),
+    
+    {reply, {ok, Acc}, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}}
 .
 
-% Define async endpoints behaviors
-handle_cast({out, Tuple}, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}) ->
-	{noreply, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}};
+%%% Define async endpoints behaviors
+handle_cast({out, Pid, Tuple}, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}) ->
+    Present = inWhiteList(WhiteListRef, Pid),
+    case Present of 
+        % If autorized try to read and wait otherwise
+        true ->
+            ets:insert(TupleSpaceRef, {Tuple}),
+            NewWaitQueue = processPendingRequests({SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue});
+        false ->
+            NewWaitQueue = WaitQueue
+    end,
+    
+    {noreply, {SyncFileRef, WhiteListRef, TupleSpaceRef, NewWaitQueue}};
 
 handle_cast({add_node, Node}, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}) ->
-	{reply, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}};
+    % Insert the node in the whitelist
+    link(Node),
+    ets:insert(WhiteListRef, {Node}),
+
+    {noreply, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}};
 
 handle_cast({rm_node, Node}, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}) ->
-	{reply, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}};
+    unlink(Node),
+    removeFromWhiteList(WhiteListRef, Node),
+    ClearedQueue = removePendingRequests(WaitQueue, Node),
+
+    {noreply, {SyncFileRef, WhiteListRef, TupleSpaceRef, ClearedQueue}};
+
+handle_cast({abort, {Type, Pid, Pattern}}, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}) ->
+    NewWaitQueue = abortPendingRequest({Type, Pid, Pattern}, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}),
+    {noreply, {SyncFileRef, WhiteListRef, TupleSpaceRef, NewWaitQueue}};
 
 handle_cast({stop}, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}) ->
-	{noreply, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}}
+    {noreply, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}}
+.
+
+
+
+%%% Auxiliary functions
+
+inWhiteList(WhiteListRef, Node) ->
+    Res = ets:match_object(WhiteListRef, {Node}),
+    case Res of
+        []        -> IsPresent = false;
+        [_H | _T] -> IsPresent = true
+    end,
+    %io:format("DEBUG PRINT - inWhiteList - Node (~p) present? (~p)\n", [Node, IsPresent]),
+    IsPresent
+.
+
+removeFromWhiteList(WhiteListRef, Node) ->
+    % Delete node from white list
+    ets:delete_object(WhiteListRef, {Node})
+.
+
+removePendingRequests(WaitQueue, Node) ->
+    ClearedQueue = lists:foldr(
+        fun({_Type, Pid, _Pattern, _Timeout}, Acc) ->
+            case Pid of
+                Node -> Acc;
+                _ -> Acc ++ [{_Type, Pid, _Pattern, _Timeout}]
+            end
+        end,
+        [],
+        WaitQueue
+    ),
+    ClearedQueue
+.
+
+processPendingRequests({SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}) ->
+    %io:format("DEBUG PRINT - processPendingRequests (start)\n", []),
+    NewWaitQueue = lists:foldr(
+		fun({Type, {Pid, Tag}, Pattern}, Acc) -> 
+			NewAcc = tryProcessRequest({Type, {Pid, Tag}, Pattern}, {SyncFileRef, WhiteListRef, TupleSpaceRef, Acc}),
+			NewAcc
+		end,
+		[],
+		WaitQueue
+	),
+    %io:format("DEBUG PRINT - processPendingRequests (end)\n", []),
+	NewWaitQueue
+.
+
+tryProcessRequest({Type, {Pid, Tag}, Pattern}, {SyncFileRef, WhiteListRef, TupleSpaceRef, WaitQueue}) ->
+    % Control  on Pattern Matching
+    Res = ets:match_object(TupleSpaceRef, {Pattern}), % MS
+    case Res of
+        % If not in the tuple space add to waitqueue
+        [] ->
+            %io:format("DEBUG PRINT - tryProcessRequest (pending)\n", []),
+            NewWaitQueue = WaitQueue ++ [{Type, {Pid, Tag}, Pattern}];
+        % Else return the element 
+        [{H} | _T] ->
+            %io:format("DEBUG PRINT - tryProcessRequest (matched)\n", []),
+            gen_server:reply({Pid, Tag}, {ok, H}),
+            case Type of
+                in -> ets:delete_object(TupleSpaceRef, {H});
+                rd -> ok
+            end,
+            NewWaitQueue = WaitQueue
+    end,
+    %io:format("DEBUG PRINT - tryProcessRequest (ended)\n", []),
+    NewWaitQueue
+.
+
+abortPendingRequest({Type, Pid, Pattern}, {_SyncFileRef, _WhiteListRef, _TupleSpaceRef, WaitQueue}) ->
+    NewWaitQueue = lists:foldr(
+		fun(Elem, Acc) ->
+            %io:format("DEBUG PRINT - abort (~p)\n", [Elem]), 
+			case Elem of
+                {Type, {Pid, _Tag}, Pattern} ->
+                    Acc;
+                _ -> Acc ++ [Elem]
+            end
+		end,
+		[],
+		WaitQueue
+	),
+	NewWaitQueue
 .
